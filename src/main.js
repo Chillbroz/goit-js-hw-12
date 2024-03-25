b1575a07cb41775336b2985e87a3cc3bb17a8b79
Chillbroz <comm.dashkovskiy@gmail.com>
2024-03-25 12:02:50 +0200

    3

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import './css/styles.css';
 
 const refs = {
   form: document.querySelector('.form'),
